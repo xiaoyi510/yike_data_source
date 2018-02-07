00608fb47551355b079f633b813531142211e5a5
Grace <15390371005@163.com>
2018-02-07 16:13:16 +0800

    no message

---
+++ application/open/home/Data1.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Grace
+ * Date: 2018/2/7
+ * Time: 15:24
+ */
+
+namespace app\open\home;
+
+
+class Data1 extends Data
+{
+    public function index()
+    {
+        $this->xu28();
+    }
+
+}
